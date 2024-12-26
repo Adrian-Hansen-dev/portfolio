cf06808dcdbcff3a33bbaed44fec975e41d4d0b6
adrianhansen <adrian.hansen@iteratec.com>
2024-12-26 20:14:49 +0100

    fixed sql schemas

--- db/001_create_schema.sql
+++ db/001_create_schema.sql
@@ -1,41 +1,41 @@
 -- delete tables and types if they already exist
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS INCLUDES;
 DROP TABLE IF EXISTS projects;
 DROP TABLE IF EXISTS skills;
+DROP TABLE IF EXISTS users;
 DROP TYPE IF EXISTS academic_title_enum;
 
 
 -- enums
-CREATE TYPE academic_title_enum AS ENUM ('Dr.', 'Prof.', 'Dipl.-Ing.', 'MBA', 'M.Sc.', 'B.Sc.');
+CREATE TYPE academic_title_enum AS ENUM ('DR', 'PROF', 'MA', 'BA', 'MSC', 'BSC');
 
 -- tables
 CREATE TABLE users
 (
     id             SERIAL PRIMARY KEY,
-    firstname      VARCHAR(100) NOT NULL,
-    lastname       VARCHAR(100) NOT NULL,
+    first_name     VARCHAR(100) NOT NULL,
+    last_name      VARCHAR(100) NOT NULL,
     academic_title academic_title_enum,
     birthdate      DATE
 );
 
 CREATE TABLE projects
 (
-    id           SERIAL PRIMARY KEY,
-    user_id      INT          NOT NULL,
-    name         VARCHAR(100) NOT NULL,
-    description  VARCHAR(200),
-    repoLink     VARCHAR(200),
-    creationDate DATE,
+    id            SERIAL PRIMARY KEY,
+    user_id       INT          NOT NULL,
+    name          VARCHAR(100) NOT NULL,
+    description   VARCHAR(200),
+    repo_link     VARCHAR(200),
+    creation_date DATE,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 CREATE TABLE skills
 (
-    id        SERIAL PRIMARY KEY,
-    user_id   INT          NOT NULL,
-    name      VARCHAR(100) NOT NULL,
-    skillDate DATE,
+    id         SERIAL PRIMARY KEY,
+    user_id    INT          NOT NULL,
+    name       VARCHAR(100) NOT NULL,
+    skill_date DATE,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 
 );
--- db/002_create_dummy_data.sql
+++ db/002_create_dummy_data.sql
@@ -10,31 +10,31 @@ FROM includes;
 
 
 -- user
-INSERT INTO users (firstname, lastname, birthdate)
+INSERT INTO users (first_name, last_name, birthdate)
 VALUES ('Adrian', 'Hansen', '01-01-2000');
 -- skills
-INSERT INTO skills (name, skilldate, user_id)
+INSERT INTO skills (name, skill_date, user_id)
 VALUES ('React', '2022-01-01', 1);
-INSERT INTO skills (name, skilldate, user_id)
+INSERT INTO skills (name, skill_date, user_id)
 VALUES ('Angular', '2021-05-01', 1);
-INSERT INTO skills (name, skilldate, user_id)
+INSERT INTO skills (name, skill_date, user_id)
 VALUES ('Java', '2020-08-01', 1);
-INSERT INTO skills (name, skilldate, user_id)
+INSERT INTO skills (name, skill_date, user_id)
 VALUES ('Python', '2020-08-01', 1);
-INSERT INTO skills (name, skilldate, user_id)
+INSERT INTO skills (name, skill_date, user_id)
 VALUES ('PostgreSQL', '2023-01-01', 1);
 
 -- projects
-INSERT INTO projects (name, description, creationdate, user_id)
+INSERT INTO projects (name, description, creation_date, user_id)
 VALUES ('Escape Game für Messeveranstaltungen', 'Internes Projekt mit einem Entwicklungzeitraum von 5 Monaten',
         '2022-01-01', 1);
-INSERT INTO projects (name, description, creationdate, user_id)
+INSERT INTO projects (name, description, creation_date, user_id)
 VALUES ('Kommunikationsplattform für Studierende', 'Kunden Projekt mit einem Entwicklungzeitraum von 10 Monaten',
         '2023-01-01',1);
-INSERT INTO projects (name, description, creationdate, user_id)
+INSERT INTO projects (name, description, creation_date, user_id)
 VALUES ('Webanwendung zur Verwaltung von Organisationseinheiten',
         'Uni Projekt mit einem Entwicklungzeitraum von 7 Monaten', '2024-01-01',1 );
-INSERT INTO projects (name, description, creationdate, user_id)
+INSERT INTO projects (name, description, creation_date, user_id)
 VALUES ('App für Logistikmanagement', 'Kunden Projekt mit einem Entwicklungzeitraum von 5 Monaten', '2023-01-01',1);
 
 -- used skills
