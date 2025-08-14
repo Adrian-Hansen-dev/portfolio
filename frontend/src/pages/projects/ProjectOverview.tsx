b7a2e1e3f78cc53f6e71580b0b2f2fd44cf35d2a
Adrian Hansen <adrian.hansen.flensburg@gmail.com>
2025-08-14 17:37:34 +0200

    adjust project overview for select and multi select

--- frontend/src/pages/projects/ProjectOverview.tsx
+++ frontend/src/pages/projects/ProjectOverview.tsx
@@ -3,6 +3,9 @@ import ProjectList from "./ProjectList.tsx";
 import CustomSelect from "../../components/Select/CustomSelect.tsx";
 import { useQuery } from "@tanstack/react-query";
 import MultiSelect from "@/components/Multi-Select/MultiSelect.tsx";
+import { ArrowDownUp, Funnel, X } from "lucide-react";
+import SearchInput from "@/components/SearchInput/SearchInput.tsx";
+import { Button } from "@/components/ui/button.tsx";
 
 function ProjectOverview() {
   const filterOptions = [
@@ -43,19 +46,34 @@ function ProjectOverview() {
 
   return (
     <div className="mx-4 flex w-full flex-col md:mx-0 md:max-w-5xl">
-      <div className="flex justify-between">
-        <MultiSelect
-          label={"Filter by"}
-          options={skillOptions}
-          value={filterBy}
-          onChange={handleFilterChange}
-          isLoading={isLoading}
-        ></MultiSelect>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center justify-start space-x-2">
+          <SearchInput></SearchInput>
+          <MultiSelect
+            label="Skills"
+            icon={<Funnel className="h-4 w-4" />}
+            options={skillOptions}
+            value={filterBy}
+            onChange={handleFilterChange}
+            isLoading={isLoading}
+          ></MultiSelect>
+          {filterBy.length !== 0 && (
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={() => setFilterBy([])}
+            >
+              <X />
+              Reset
+            </Button>
+          )}
+        </div>
         <CustomSelect
           onChange={handleSortChange}
           options={filterOptions}
           value={sortBy}
-          label={"Sort by"}
+          label="Sort by"
+          icon={<ArrowDownUp className="h-4 w-4" />}
         ></CustomSelect>
       </div>
       <ProjectList sortBy={sortBy} filterBySkill={filterBy}></ProjectList>
