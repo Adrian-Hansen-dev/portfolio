22615d30d483c414a8b9bbc1f1e75bd4af34631b
Adrian Hansen <adrian.hansen.flensburg@gmail.com>
2025-08-22 21:10:11 +0200

    add about dummy content

--- frontend/src/pages/about/AboutOverview.tsx
+++ frontend/src/pages/about/AboutOverview.tsx
@@ -2,9 +2,24 @@ export default function AboutOverview() {
   return (
     <section
       id="about"
-      className="my-4 h-screen w-full scroll-mt-20 rounded-2xl bg-gray-100 p-10 md:mx-0 md:max-w-5xl"
+      className="bg-card my-4 h-screen w-full scroll-mt-20 rounded-2xl p-10 md:mx-0 md:max-w-5xl"
     >
-      <h1 className="h1">About</h1>
+      <h1 className="h1">Adrian Hansen</h1>
+      <h2 className="h3">Full Stack Engineer</h2>
+      <p className="p">
+        I build accessible, pixel-perfect digital experiences for the web.
+      </p>
+      <p className="p2">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet.
+      </p>
     </section>
   );
 }
