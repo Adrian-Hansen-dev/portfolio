0359aa1842739267635cea9500a7b6bd45c91270
Adrian Hansen <adrian.hansen.flensburg@gmail.com>
2025-08-23 17:16:33 +0200

    adjust navbar layout

--- frontend/src/pages/navigation/Navbar.tsx
+++ frontend/src/pages/navigation/Navbar.tsx
@@ -5,18 +5,20 @@ import { ModeToggle } from "@/components/ui/mode-toggle.tsx";
 
 export default function Navbar() {
   return (
-    <nav className="max-w-screen-5xl bg-background/30 fixed inset-x-5 top-2 z-10 h-16 rounded-2xl border backdrop-blur-lg backdrop-filter">
+    <nav className="max-w-screen-5xl bg-background/30 fixed inset-x-5 top-2 z-10 h-16 rounded-2xl border px-4 backdrop-blur-lg backdrop-filter">
       <div className="mx-auto flex h-full items-center justify-between px-4">
         <Logo />
 
         {/* Desktop Menu */}
         <NavMenu className="hidden text-sm md:block"></NavMenu>
 
-        <ModeToggle />
+        <div className="flex items-center gap-2">
+          <ModeToggle />
 
-        {/* Mobile Menu */}
-        <div className="md:hidden">
-          <NavSheet />
+          {/* Mobile Menu */}
+          <div className="md:hidden">
+            <NavSheet />
+          </div>
         </div>
       </div>
     </nav>
