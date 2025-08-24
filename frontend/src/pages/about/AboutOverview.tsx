ae8298dd601d3f65cf11d2a839639b040db109d5
Adrian Hansen <adrian.hansen.flensburg@gmail.com>
2025-08-24 12:53:08 +0200

    add request for about section

--- frontend/src/pages/about/AboutOverview.tsx
+++ frontend/src/pages/about/AboutOverview.tsx
@@ -1,25 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import Loader from "@/components/custom/Loader.tsx";
+
 export default function AboutOverview() {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:8080/user/1");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    },
+  });
+
   return (
     <section
       id="about"
       className="bg-card my-4 h-screen w-full scroll-mt-20 rounded-2xl p-10 md:mx-0 md:max-w-5xl"
     >
-      <h1 className="h1">Adrian Hansen</h1>
-      <h2 className="h3">Full Stack Engineer</h2>
-      <p className="p">
-        I build accessible, pixel-perfect digital experiences for the web.
-      </p>
-      <p className="p2">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet.
-      </p>
+      {isLoading ? (
+        <Loader />
+      ) : isError ? (
+        <p>Error: {error.message}</p>
+      ) : (
+        <>
+          <h2 className="h2">
+            {data.firstName} {data.lastName}
+          </h2>
+          <h3 className="h3">{data.profession}</h3>
+          <p className="p">{data.description}</p>
+        </>
+      )}
     </section>
   );
 }
