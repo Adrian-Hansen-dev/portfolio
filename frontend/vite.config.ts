99d895f1a760f1fbb8debef4958a05be6e0cfb9e
Adrian Hansen <adrian.hansen.flensburg@gmail.com>
2025-08-30 07:58:47 +0200

    add frontend deployment scripts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: "/portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
