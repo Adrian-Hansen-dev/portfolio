6d4eda9b8e17c4a2c1d0b1197809f8e58ba90b40
Adrian Hansen <adrian.hansen.flensburg@gmail.com>
2026-03-29 22:13:57 +0200

    adjust db scripts

--- backend/src/main/resources/db/migration/V2__add_read_only_rls.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE users ENABLE ROW LEVEL SECURITY;
-
-CREATE POLICY "Allow public read access"
-    ON users
-    FOR SELECT
-    USING (true);
-
--- disable mutation
-REVOKE INSERT, UPDATE, DELETE ON users FROM anon, authenticated;
